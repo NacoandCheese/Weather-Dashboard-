66a2f55ffb8d035f0d19f906d77601816891f489
Nicolo Danna <ndanna128@gmail.com>
2022-02-10 17:18:24 -0500

    test fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,5 @@
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}")
-    
+//Api key
+var apiKey = "2d092163794fc6f4624b18ac257ab93a"
+var cityName = 'Los Angeles'
+
+fetch ('http://api.openweathermap.org/geo/1.0/direct?q=+cityName+&appid=+2d092163794fc6f4624b18ac257ab93a')
