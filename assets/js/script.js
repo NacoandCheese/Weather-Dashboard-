878173c4f8890c1d8a62c3d124cfb7c4a891eb06
Nicolo Danna <ndanna128@gmail.com>
2022-02-10 18:25:04 -0500

    added working api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 //Api key
 var apiKey = "2d092163794fc6f4624b18ac257ab93a"
-var cityName = 'Los Angeles'
+var cityName = 'Wrightstown'
 
-fetch ('http://api.openweathermap.org/geo/1.0/direct?q=+cityName+&appid=+2d092163794fc6f4624b18ac257ab93a')
+fetch ('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=2d092163794fc6f4624b18ac257ab93a')
+    .then(response => response.json())
+    .then(data => {
+        lat = data[0].lat
+        lon = data[0].lon
+        console.log(data);
+    });
