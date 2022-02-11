a7adc93c2e07fa2f527e41ba80216c5dc099493a
Nicolo Danna <ndanna128@gmail.com>
2022-02-11 14:50:01 -0500

    added script and links added html to resemble mockup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 //Api key
 var apiKey = "2d092163794fc6f4624b18ac257ab93a"
-var cityName = 'Wrightstown'
+var cityName = document.querySelector
 
 fetch ('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=2d092163794fc6f4624b18ac257ab93a')
     .then(response => response.json())
