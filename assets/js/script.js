ceeda04e4ff4b9b0e60cc176c2325c4e7897d642
Nicolo Danna <ndanna128@gmail.com>
2022-02-12 22:16:19 -0500

    tried adding js couldnt get api working

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,19 +14,48 @@ function initPage() {
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
 
-    const apiKey = "2d092163794fc6f4624b18ac257ab93a"
+
+    const apiKey = "2d092163794fc6f4624b18ac257ab93a";
+
+    function getWeather(cityName) {
+
+        let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "appid=" + apiKey;
+        axios.get(queryURL)
+        .then(function (response) {
+
+            todayweatherEl.classList.remove("d-none");
+
+            const currentDate = new Date(response.data.dt * 1000);
+            const day = currentDate.getDate();
+            const month = getMonth(); + 1;
+            const year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+
+
+            //Get UV index
+            let lat = response.data.coord.lat;
+            let lon = response.data.coord.lonl;
+            let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon" + lon + "&appid=" + apiKey +"&cnt=1";
+            axios.get(UVQueryURL)
+            .then(function (response) {
+                let UVindex = document.createElement("span");
+                
+            })
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
 // var apiKey = "2d092163794fc6f4624b18ac257ab93a"
 // var cityName = document.querySelector
 
